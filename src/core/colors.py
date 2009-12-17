d4a3f4738c4452bff19c9a6eea665d6d09e13f84
Chris Willard <chriswillarddesign@GMail.com>
2009-12-17 03:37:07 +0000

    Adding colors.py module.

---
+++ src/core/colors.py
@@ -0,0 +1,13 @@
+# Generic Color Constants
+COLOR_BLACK   = (0, 0, 0)
+COLOR_GREEN   = (0, 255, 0)
+COLOR_YELLOW  = (255, 255, 0)
+COLOR_RED     = (255, 0, 0)
+COLOR_WHITE   = (255, 255, 255)
+COLOR_PURPLE  = (180, 0, 180)
+
+# Specific Object Color Constants
+COLOR_FROG             = (0, 160, 0)
+COLOR_BACKGROUND_GREEN = (79, 116, 32)
+COLOR_NAVY_BLUE        = (29, 41, 90)
+COLOR_PALE_YELLOW      = (255, 244, 86)
